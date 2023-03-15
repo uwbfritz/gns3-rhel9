54538ece2ff280b0dec6eab2d97c46211b5e3ba4
Bill <bfritz@uw.edu>
2023-03-15 14:51:54 -0700

    15/03/2023_14:51:54 Working in: /work/script-repository/general/gns3-rhel9 Committed by: bfritz

--- scripts/rdp-generator.sh
+++ scripts/rdp-generator.sh
@@ -1,5 +1,17 @@
 #!/bin/bash
 
+# Check if the script is being run as root
+if [ "$EUID" -ne 0 ]; then
+    echo "Please run as root"
+    exit 1
+fi
+
+# Check if a username was passed as an argument
+if [ -z "$1" ]; then
+    echo "Usage: $0 <username>"
+    exit 1
+fi
+
 # Read the username from command line arguments
 USERNAME=$1
 
