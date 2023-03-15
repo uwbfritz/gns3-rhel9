41bd80ca8a4867eabc9214b44bdfec82af3e296b
Bill <bfritz@uw.edu>
2023-03-15 14:57:39 -0700

    15/03/2023_14:57:39 Working in: /work/script-repository/general/gns3-rhel9 Committed by: bfritz

---
+++ scripts/install.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+
+INSTALL_DIR="$HOME/install"
+
+mkdir -p "$INSTALL_DIR"
+
+curl -L https://github.com/uwbfritz/gns3-rhel9/archive/master.tar.gz | tar xz -C "$INSTALL_DIR" --strip-components=1
+
+cd "$INSTALL_DIR" || exit 1
+
+bash rhel9-gns3-install.sh
